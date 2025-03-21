8061f272d7eea8d630e804b04d345ce1389269ff
Eduardo Lago <laguillo@gmail.com>
2025-03-21 13:22:53 -0500

    WIP

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,16 @@
 import '@/app/global.css';
-import { Geist } from 'next/font/google';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({
-  children,
+  children
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang='en'>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,30 @@
-import { Button } from "@/components/ui/button";
-import { ArrowRight } from "lucide-react";
-import Link from "next/link";
+import { Button } from '@/components/ui/button';
+import { ArrowRight } from 'lucide-react';
+import Link from 'next/link';
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+    <main className='flex min-h-screen flex-col p-6'>
+      <div className='flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52'>
         {/* <AcmeLogo /> */}
       </div>
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black" />
+      <div className='mt-4 flex grow flex-col gap-4 md:flex-row'>
+        <div className='flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20'>
+          <div className='relative h-0 w-0 border-r-[15px] border-b-[26px] border-l-[15px] border-r-transparent border-b-black border-l-transparent' />
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{" "}
-            <Link href="https://nextjs.org/learn/" className="text-blue-500">
+            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <Link href='https://nextjs.org/learn/' className='text-blue-500'>
               Next.js Learn Course
             </Link>
             , brought to you by Vercel.
           </p>
-          <Button asChild size={"lg"} className="w-fit">
-            <Link href="/login">
-              <span>Log in</span> <ArrowRight className="size-4" />
+          <Button asChild size={'lg'} className='w-fit'>
+            <Link href='/login'>
+              <span>Log in</span> <ArrowRight className='size-4' />
             </Link>
           </Button>
         </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+        <div className='flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12'>
           {/* Add Hero Images Here */}
         </div>
       </div>
